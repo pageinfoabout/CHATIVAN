9005b5b4e26c44da643cc84c167bb6345cd5db1d
Ivan Gorskiy <gorskii241152003@gmail.com>
2025-10-10 14:43:41 +0300

    wip

--- server/configs/imageKit.js
+++ server/configs/imageKit.js
@@ -1,7 +1,7 @@
-import Imagekit from 'imagekit';
+import ImageKit from 'imagekit';
 
 
-const imagekit = new Imagekit({
+const imagekit = new ImageKit({
     publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
     privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
     urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
