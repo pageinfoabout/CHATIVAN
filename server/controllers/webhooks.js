8f4fee3e253a212a93c264ceaf6a21b039d3cdaa
Ivan Gorskiy <gorskii241152003@gmail.com>
2025-10-14 22:54:18 +0300

    Describe recent changes

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,7 +1,6 @@
 import { YooCheckout } from '@a2seven/yoo-checkout';
-import token from '../middlewares/auth.js'
 
-const checkout = new YooCheckout({ shopId: process.env.YOOKASSA_SHOP_ID, secretKey: process.env.YOOKASSA_SECRET_KEY, token: token });
+const checkout = new YooCheckout({ shopId: process.env.YOOKASSA_SHOP_ID, secretKey: process.env.YOOKASSA_SECRET_KEY});
 try {
     const webHookList = await checkout.getWebHookList();
     console.log(webHookList)
