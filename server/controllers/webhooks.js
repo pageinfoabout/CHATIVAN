ff28fda4633b6fc24f15259a30de90037c1d674a
Ivan Gorskiy <gorskii241152003@gmail.com>
2025-10-14 23:07:31 +0300

    fds

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -3,11 +3,12 @@ import YooKassa from 'yookassa'
 
 export const webhooks = async (req, res) => {
     try {
-        const yookassa = new YooKassa({
+        const yooKasa = new YooKassa({
             shopId: process.env.YOOKASSA_SHOP_ID,
             secretKey: process.env.YOOKASSA_SECRET_KEY
         })
-        console.log(yookassa)
+        const response = await yooKasa.getWebHookList()
+        console.log(response)
     } catch (error) {
         console.log(error)
     }
