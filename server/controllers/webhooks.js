0816ee58fde17f01598715b931decb224f08278e
Ivan Gorskiy <gorskii241152003@gmail.com>
2025-10-14 23:38:51 +0300

    sdds

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,18 +1,17 @@
 import YooKassa from 'yookassa'
 
+import Transaction from '../models/Transaction.js'
+import User from '../models/User.js'
 
+
+
+
+    
 export const webhooks = async (req, res) => {
-    try {
-        const yookassa = new YooKassa({
-            shopId: process.env.YOOKASSA_SHOP_ID,
-            secretKey: process.env.YOOKASSA_SECRET_KEY
-        })
-        const response = await yookassa.webhook(req.body)
-        console.log(response)
-    } catch (error) {
-        console.log(error)
-    }
-    
-    
-    
+    const yookassa = new YooKassa({
+        shopId: process.env.YOOKASSA_SHOP_ID,
+        secretKey: process.env.YOOKASSA_SECRET_KEY
+    })
+    const signature = req.body.event;
+    console.log(signature)
 }
